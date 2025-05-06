5e50e7686bd1cc86a857914b0f3e6b5c2a02a8bc
exkfont <kamal.fontirroig@externos.bci.cl>
2025-05-06 10:38:53 -0400

    Avance

--- src/main/java/com/banco/apiusuarios/modelo/Telefono.java
+++ src/main/java/com/banco/apiusuarios/modelo/Telefono.java
@@ -1,8 +1,7 @@
 package com.banco.apiusuarios.modelo;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.ToString;
 
--- src/main/java/com/banco/apiusuarios/modelo/Usuario.java
+++ src/main/java/com/banco/apiusuarios/modelo/Usuario.java
@@ -1,8 +1,8 @@
 package com.banco.apiusuarios.modelo;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
